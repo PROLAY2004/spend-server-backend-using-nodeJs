abf247279f6cf68a9b8e4adb57f4ea9f80807b51
Prolay Halder <prolayhalder2004@gmail.com>
2026-07-08 14:33:46 +0530

    Feat : contatct controller done

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,8 +1,16 @@
+import SendEmailService from '../services/sendMailService.js';
+
+const mailer = new SendEmailService();
+
 export default class UserController {
   contact = async (req, res, next) => {
     try {
+      const { name, email, message } = req.body;
+
+      await mailer.contactMailer(name, email, message);
+
       res.status(200).json({
-        message: 'Contact information fetched successfully',
+        message: 'Response Submitted Successfully',
         success: true,
         data: {
           user: req.user,
