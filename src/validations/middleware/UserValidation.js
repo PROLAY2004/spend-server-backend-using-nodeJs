ac81f5c60490ddaa80131ce08fb97f81ee07a58b
Prolay Halder <prolayhalder2004@gmail.com>
2026-07-08 13:17:15 +0530

    feat : contact form validation middleware added

---
+++ src/validations/middleware/UserValidation.js
@@ -0,0 +1,19 @@
+import UserSchema from '../schema/UserSchema.js';
+import { ValidationError } from 'yup';
+
+const schema = new UserSchema();
+
+export default class UserValidation {
+  contactRequest = async (req, res, next) => {
+    try {
+      await schema.contactSchema.validate(req.body, {
+        abortEarly: true, // don't return all validation errors
+        stripUnknown: true, // remove unexpected fields
+      });
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+}
