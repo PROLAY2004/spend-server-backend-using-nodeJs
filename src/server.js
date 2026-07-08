710f9ec3224d43b429ac72b5d31ae1cee4a048e3
Prolay Halder <prolayhalder2004@gmail.com>
2026-07-08 12:58:23 +0530

    feat : error handler file added

--- src/server.js
+++ src/server.js
@@ -4,15 +4,8 @@ import cors from 'cors';
 import configuration from './config/config.js';
 import errorHandler from './error/errorHandler.js';
 import loggerMiddleware from './validations/middleware/loggerMiddleware.js';
-import connectDB from './config/dbConfig.js';
-import adminRoutes from './routes/adminRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import TokenValidation from './validations/middleware/TokenValidation.js';
+// import connectDB from './config/dbConfig.js';
 
-await connectDB();
-
-const tokenValidator = new TokenValidation();
 const app = express();
 
 app.use(cors(configuration.CORS));
@@ -20,12 +13,12 @@ app.use(cors(configuration.CORS));
 app.use(express.json());
 // app.use(loggerMiddleware);
 
-app.use('/user/auth', authRoutes);
-app.use('/user/admin', tokenValidator.isAdmin, adminRoutes);
-app.use('/user', userRoutes);
+// app.use('/user/auth', authRoutes);
+// app.use('/user/admin', tokenValidator.isAdmin, adminRoutes);
+// app.use('/user', userRoutes);
 
 app.use(errorHandler);
 
 app.listen(configuration.PORT, () => {
-  console.log(`sastaMovies listening on port ${configuration.PORT}`);
+  console.log(`spendServer listening on port ${configuration.PORT}`);
 });
