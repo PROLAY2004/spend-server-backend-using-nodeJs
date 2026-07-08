4b8052ae5d1f04ce617e2453dbc05888097451c3
Prolay Halder <prolayhalder2004@gmail.com>
2026-07-08 12:55:40 +0530

    feat : config file added

---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const configuration = {
+  PORT: process.env.PORT,
+//   MONGO_URI: process.env.MONGO_URI,
+  FRONTEND_URL: process.env.FRONTEND_URL,
+//   MAIL_SERVICE: process.env.MAIL_SERVICE,
+//   MAIL_USER: process.env.MAIL_USER,
+//   MAIL_PASS: process.env.MAIL_PASS,
+//   ACCESS_SECRET: process.env.ACCESS_SECRET,
+//   REFRESH_SECRET: process.env.REFRESH_SECRET,
+//   ACCESS_EXPIRE: process.env.ACCESS_EXPIRE,
+//   REFRESH_EXPIRE: process.env.REFRESH_EXPIRE,
+  CORS: {
+    origin: [process.env.FRONTEND_URL],
+    methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE'],
+  },
+};
+
+export default configuration;
