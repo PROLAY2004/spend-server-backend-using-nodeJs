7efd11843388d8706ed24588f42092b8ba199ab5
Prolay Halder <prolayhalder2004@gmail.com>
2026-07-20 14:40:35 +0530

    fix : add payer controller bug fixed

--- src/controller/PayerController.js
+++ src/controller/PayerController.js
@@ -1,23 +1,19 @@
-import payer from "../models/payerModel.js";
+import payer from '../models/payerModel.js';
 
 export default class PayerController {
   addPayer = async (req, res, next) => {
     try {
       const { name, mobile } = req.body;
-      const user = req.user;
 
       await payer.create({
         name,
         mobile,
-        userId : user._id
-      })
+        userId: req.user._id,
+      });
 
       res.status(200).json({
         message: 'New Payer Added Successfully',
         success: true,
-        data: {
-          user,
-        },
       });
     } catch (err) {
       next(err);
